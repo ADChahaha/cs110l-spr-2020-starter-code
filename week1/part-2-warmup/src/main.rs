ad80f38f9c48e24f84b7d493bd560560e822c559
ADChahaha <3383642310a@gmail.com>
2026-06-28 16:22:08 +0800

    complete week1 part-1 and part-2

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -1,23 +1,39 @@
 /* The following exercises were borrowed from Will Crichton's CS 242 Rust lab. */
 
-use std::collections::HashSet;
+use std::{collections::HashSet};
 
 fn main() {
     println!("Hi! Try running \"cargo test\" to run tests.");
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut new_v: Vec<i32> = Vec::new();
+    for num in v.iter() {
+        new_v.push(num + n);
+    }
+    new_v
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for num in v.iter_mut() {
+        *num = *num + n;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut set: HashSet<i32> = HashSet::new();
+    let mut i = 0;
+    while i < v.len(){
+        let num = v[i];
+        if set.contains(&num){
+            v.remove(i);
+        }
+        else{
+            set.insert(num);
+            i = i + 1;
+        }
+    }
 }
-
 #[cfg(test)]
 mod test {
     use super::*;
