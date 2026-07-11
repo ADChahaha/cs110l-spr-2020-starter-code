ce7bcb9322b987d2286e8dd22d1e5de2a5e07e3f
ADChahaha <3383642310a@gmail.com>
2026-07-11 22:40:56 +0800

    implement generic and trait 'Clone' and 'PartialEq' of LinkedList

--- list/src/main.rs
+++ list/src/main.rs
@@ -1,15 +1,18 @@
-struct LinkedList {
-    head: Option<Box<Node>>,
+use core::fmt;
+use std::str::FromStr;
+
+struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
 }
 
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
         Node {
             value: value,
             next: next,
@@ -17,8 +20,8 @@ impl Node {
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
         LinkedList {
             head: None,
             size: 0,
@@ -33,19 +36,23 @@ impl LinkedList {
         self.size == 0
     }
 
-    pub fn push(&mut self, value: u32) {
+    pub fn push(&mut self, value: T) {
         let new_node = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
 
-    pub fn pop(&mut self) -> Option<u32> {
+    pub fn pop(&mut self) -> Option<T> {
         let head = self.head.take()?;
         self.head = head.next;
         self.size -= 1;
         Some(head.value)
     }
-
+}
+impl<T> LinkedList<T>
+where
+    T: fmt::Display,
+{
     pub fn display(&self) {
         let mut current_node = &self.head;
         while let Some(node) = current_node {
@@ -54,15 +61,81 @@ impl LinkedList {
         }
         println!(" ")
     }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut current = &self.head;
+        let mut result = String::new();
+        loop {
+            match current {
+                Some(node) => {
+                    result = format!("{} {}", result, node.value);
+                    current = &node.next;
+                }
+                None => break,
+            }
+        }
+        write!(f, "{}", result)
+    }
+}
+impl<T> Clone for LinkedList<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        let mut new_list = Self {
+            head: None,
+            size: 0,
+        };
+        let mut arr: Vec<T> = Vec::new();
+        let mut current = &self.head;
+        while let Some(node) = current {
+            arr.push(node.value.clone());
+            current = &node.next;
+        }
+        while let Some(value) = arr.pop() {
+            new_list.push(value);
+        }
+        new_list
+    }
+}
+
+impl<T> PartialEq for LinkedList<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        if self.size != other.size {
+            return false;
+        }
+        let mut current_node1 = &self.head;
+        let mut current_node2 = &other.head;
+        while let Some(node1) = current_node1
+            && let Some(node2) = current_node2
+        {
+            if node1.value != node2.value {
+                return false;
+            }
+            current_node1 = &node1.next;
+            current_node2 = &node2.next;
+        }
+        true
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        let mut current = self.head.take();
+        while let Some(mut node) = current {
+            current = node.next.take();
+        }
+    }
 }
 
 fn main() {
-    let mut list: LinkedList = LinkedList::new();
-    for i in 0..10 {
-        list.push(i);
-    }
-    list.display();
+    let mut list: LinkedList<String> = LinkedList::new();
+    list.push(String::from_str("jdj").unwrap());
+    list.push(String::from_str("jdj").unwrap());
+    let list2 = list.clone();
     list.pop();
     list.display();
-    println!("{}", list.get_size());
+    list2.display();
 }
